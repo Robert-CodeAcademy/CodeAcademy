4a3593e585141929384040529ee8dd3773446aed
Robert-CodeAcademy <108361159+Robert-CodeAcademy@users.noreply.github.com>
2024-01-28 20:55:28 +0100

    Add files via upload

---
+++ laravelCRUDproject/storage/framework/views/9286a10db0cf8dfd37defdb78856b620.php
@@ -0,0 +1,35 @@
+<!-- resources/views/people/create.blade.php -->
+
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="container">
+        <h2>Create New Person</h2>
+        <form method="POST" action="<?php echo e(route('people.store')); ?>">
+            <?php echo csrf_field(); ?>
+            <div class="form-group">
+                <label for="first_name">First Name</label>
+                <input type="text" class="form-control" id="first_name" name="first_name" required>
+            </div>
+            <div class="form-group">
+                <label for="last_name">Last Name</label>
+                <input type="text" class="form-control" id="last_name" name="last_name" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="phone_number">Phone Number</label>
+                <input type="text" class="form-control" id="phone_number" name="phone_number" required>
+            </div>
+            <div class="form-group">
+                <label for="city">City</label>
+                <input type="text" class="form-control" id="city" name="city" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\robert_crud_project\resources\views/people/create.blade.php ENDPATH**/ ?>
